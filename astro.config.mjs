c2ab18273213817aff7b13c0450b7d02ccb24ea3
y0nei <y0nei@proton.me>
2024-06-16 20:33:16 +0200

    feat: Install astro-icon & update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
+import icon from "astro-icon";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+    integrations: [
+        svelte(),
+        icon({ iconDir: "public/icons" })
+    ]
 });
