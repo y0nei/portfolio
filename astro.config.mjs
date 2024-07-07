e4393e1e229e06b101880e1396e4cb3ecf93f4ea
y0nei <y0nei@proton.me>
2024-07-07 16:58:29 +0200

    feat: Add yaml parser and move to .yml for socials

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import icon from "astro-icon";
+import yaml from '@rollup/plugin-yaml';
 
 // https://astro.build/config
 export default defineConfig({
+    vite: {
+      plugins: [yaml()]
+    },
     integrations: [
         svelte(),
         icon({ iconDir: "public/icons" })
