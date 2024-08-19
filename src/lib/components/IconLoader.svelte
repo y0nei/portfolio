09b3971df0c821f4810fc9e5bd9abdb4537846ba
y0nei <y0nei@proton.me>
2024-08-19 20:35:06 +0200

    feat: Custom SVG icon loader

---
+++ src/lib/components/IconLoader.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    export let name: string;
+    export let size: number = 28;
+
+    let iconMap = new Map<string, string>()
+
+    const icons = import.meta.glob(
+        "$icons/*.svg",
+        { eager: true, query: "?raw", import: "default" }
+    )
+
+    for (const obj in icons) {
+        const key: string = obj.slice(14, -4);
+        iconMap.set(key, icons[obj] as string)
+    }
+</script>
+
+<div style:width={size+"px"} style:height={size+"px"}>
+    {@html iconMap.get(name)}
+</div>
+
+<style lang="scss">
+    div :global(svg) {
+        fill: inherit;
+    }
+</style>
