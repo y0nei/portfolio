e01c7511779fc09e202b56515af0c05c732650b4
y0nei <y0nei@proton.me>
2024-08-19 20:34:39 +0200

    feat: Re-add sections component

---
+++ src/lib/layouts/Sections.svelte
@@ -0,0 +1,32 @@
+<div class="section-wrapper">
+    <slot />
+</div>
+
+<style lang="scss" global>
+    .section-wrapper {
+        scroll-behavior: smooth;
+        overflow-y: scroll;
+
+        section {
+            height: 100vh;
+            width: 100vw;
+
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: relative;
+        }
+    }
+
+    section#home {
+        background-color: var(--background-color);
+
+        .home-wrapper {
+            display: flex;
+            justify-content: space-evenly;
+            width: 100%;
+            margin: 0 2rem;
+            gap: 5rem;
+        }
+    }
+</style>
