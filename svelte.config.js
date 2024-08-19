874bf40317e5c17115d874a4dff980425125e5f7
y0nei <y0nei@proton.me>
2024-08-19 20:31:13 +0200

    feat: Alias for icon directory

--- svelte.config.js
+++ svelte.config.js
@@ -9,11 +9,10 @@ const config = {
 	preprocess: sveltePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		alias : {
+			"$icons": "static/icons"
+		}
 };
 
 export default config;
