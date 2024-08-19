fa89e036a22d6d931d1de834781509dd59ce8434
y0nei <y0nei@proton.me>
2024-08-19 20:33:39 +0200

    feat: YAML parser, CSS reset & better preprocessor

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,16 @@
 import adapter from '@sveltejs/adapter-auto';
-// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { sveltePreprocess } from 'svelte-preprocess';
+import path from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: sveltePreprocess(),
-
-	kit: {
+	kit: { 
 		adapter: adapter(),
 		alias : {
 			"$icons": "static/icons"
 		}
+	},
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import ViteYaml from "@modyfi/vite-plugin-yaml";
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), ViteYaml()]
 });
