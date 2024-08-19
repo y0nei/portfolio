4935440d7bed6b7c0a4a4abf2e8360bc3c4e7fb4
y0nei <y0nei@proton.me>
2024-08-19 20:35:26 +0200

    feat: Re-add info icon component

---
+++ src/lib/components/InfoIcon.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+import Icon from "$lib/components/IconLoader.svelte";
+</script>
+
+<span class="info-icon {$$props.class || ''}">
+    <Icon name="question-mark" />
+    <slot />
+</span>
+
+<style lang="scss">
+    .info-icon {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+        width: 1.2rem;
+        height: 1.2rem;
+        padding: 5px;
+        border-radius: 100%;
+        color: var(--background-color);
+        background-color: var(--offwhite-color);
+        box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
+        user-select: none;
+
+        b { font-size: 0.75rem }
+    }
+</style>
