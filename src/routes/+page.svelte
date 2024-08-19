9a2142a512af9d950f85d64575cf6475d3bf7f96
y0nei <y0nei@proton.me>
2024-08-19 20:34:22 +0200

    feat: Re-add CSS variables

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,24 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <style lang="scss">
-    h1 {
-        background-color: beige;
+    @font-face {
+		font-family: "Rubik";
+		src: url("/fonts/Rubik-VariableFont_wght.ttf") format("truetype");
+		font-display: swap;
+	}
+
+	:root {
+        /* Other cool colors: #233531 #F3EEE8 */
+
+		--accent-color: #BBD38B;
+        --accent-color-dark: #30422E;
+        --secondary-color: #8BBBD3;  /* Triadic to accent */
+
+		--offwhite-color: #F9F4EA;
+		--background-color: #1F1F21;
+		--background-color-alt: #3A393B;
+
+		--text-color: var(--offwhite-color);
+        --border-radius: 0.75rem;
     }
 </style>
