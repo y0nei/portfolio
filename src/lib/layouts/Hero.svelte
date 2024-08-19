25988b36f45a8ca92e9c8c07bc29a6057f51737a
y0nei <y0nei@proton.me>
2024-08-19 20:36:48 +0200

    feat: Re-add hero section component

---
+++ src/lib/layouts/Hero.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import HeroHeader from "$lib/components/HeroHeader.svelte";
+    import HeroSocials from "$lib/components/HeroSocials.svelte";
+</script>
+
+<div class="hero-header-wrapper">
+    <HeroHeader />
+    <HeroSocials />
+</div>
+
+<style lang="scss">
+    .hero-header-wrapper {
+        display: flex;
+        flex-direction: column;
+        gap: 1.5rem;
+    }
+
+    @media only screen and (max-width: 1337px) {
+        .hero-header-wrapper {
+            width: 100%;
+        }
+    }
+</style>
