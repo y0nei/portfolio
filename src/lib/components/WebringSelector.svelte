019093e130f41a21b3f438f8ecc5dc8d1d08c73e
y0nei <y0nei@proton.me>
2024-09-25 23:07:31 +0200

    feat: Webring selector and two webrings

---
+++ src/lib/components/WebringSelector.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+    export let name: string;
+    export let href: Href = {
+        next: '',
+        previous: '',
+        random: ''
+    };
+
+    interface Href {
+        next: string;
+        previous: string;
+        random: string;
+    }
+</script>
+
+<div>
+    <p>{name}</p>
+    <span>
+        <a class="webring-previous" href={href.previous}>&laquo;</a>
+        <a class="webring-random" href={href.random}>Random</a>
+        <a class="webring-next" href={href.random}>&raquo;</a>
+    </span>
+</div>
+
+<style lang="scss">
+    div {
+        background-color: #454549;
+        border-radius: calc(var(--border-radius) - 5px);
+        padding: 5px;
+        font-size: 0.75rem;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 0.25rem;
+        height: fit-content;
+        width: fit-content;
+        box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.2);
+
+        span {
+            display: flex;
+            justify-content: center;
+
+            a {
+                padding: 2px 5px;
+                background-color: gray;
+                user-select: none;
+                transition: 150ms;
+                text-decoration: none;
+                color: var(--offwhite-color);
+
+                &:hover {
+                    background-color: var(--secondary-color);
+                    cursor: pointer;
+                    color: var(--background-color);
+                }
+
+                &:first-child {
+                    border-right: 2px solid #454549;
+                    border-top-left-radius: calc(var(--border-radius) - 7px);
+                    border-bottom-left-radius: calc(var(--border-radius) - 7px);
+                }
+                &:last-child {
+                    border-left: 2px solid #454549;
+                    border-top-right-radius: calc(var(--border-radius) - 7px);
+                    border-bottom-right-radius: calc(var(--border-radius) - 7px);
+                }
+            }
+        }
+    }
+</style>
--- src/lib/layouts/BentoGrid.svelte
+++ src/lib/layouts/BentoGrid.svelte
@@ -2,6 +2,7 @@
     import BentoItem from "$lib/components/BentoItem.svelte";
     import Handles from "$lib/layouts/Handles.svelte";
     import BadgeMarquee from "$lib/components/BadgeMarquee.svelte";
+    import WebringSelector from "$lib/components/WebringSelector.svelte";
 </script>
 
 <div class="bento-grid">
@@ -14,6 +15,18 @@
     </BentoItem>
     <BentoItem name="webrings">
         <p slot="header">Webrings</p>
+        <div class="webring-wrapper">
+            <WebringSelector name="CSS Joy"
+                href.previous="https://webri.ng/webring/cssjoy/previous?via=https://yonei.dev/"
+                href.random="https://webri.ng/webring/cssjoy/random?via=https://yonei.dev/"
+                href.next="https://webri.ng/webring/cssjoy/next?via=https://yonei.dev/"
+            />
+            <WebringSelector name="Fediring"
+                href.previous="https://fediring.net/previous?yonei.dev"
+                href.random="https://fediring.net/random"
+                href.next="https://fediring.net/next?host=yonei.dev"
+            />
+        </div>
     </BentoItem>
     <BentoItem name="buttons" hollow/>
 </div>
@@ -25,5 +38,15 @@
         grid-template-rows: repeat(2, 1fr);
         gap: 1rem;
         width: 100%;
+
+        .webring-wrapper {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 5px;
+            margin-top: 0;
+            flex: 1;
+            gap: 0.5rem;
+        }
     }
 </style>
