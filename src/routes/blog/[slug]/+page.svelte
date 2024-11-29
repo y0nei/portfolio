a8aa32a663792d2fcb9ef401b5316c1274e8d01a
y0nei <y0nei@proton.me>
2024-11-29 17:57:49 +0100

    feat: Sample svelte component for post rendering

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { PageData } from "./$types";
+
+	let { data }: { data: PageData } = $props();
+</script>
+
+<svelte:head>
+	<title>{data.meta.title}</title>
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content={data.meta.title} />
+</svelte:head>
+
+<article>
+	<hgroup>
+		<h1>{data.meta.title}</h1>
+		<p>Published at {data.meta.date}</p>
+	</hgroup>
+
+	<div class="tags">
+		{#each data.meta.tags as tag}
+			<span>&num;{tag}</span>
+		{/each}
+	</div>
+
+	<div class="content">
+		<data.content />
+	</div>
+</article>
