8f73bb3206e125db9ed33a0b2e7e3b1508a37e85
y0nei <y0nei@proton.me>
2024-12-01 13:38:21 +0100

    feat: Blog route with post listing

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import type { PageData } from "./$types";
+
+	let { data }: { data: PageData } = $props()
+</script>
+
+<svelte:head>
+	<title>Yonei's Blog</title>
+</svelte:head>
+
+<ul>
+	{#each data.posts as post}
+		<li>
+			<a href="blog/{post.slug}">{post.title}</a>
+			<p class="date">{post.date}</p>
+			{#if post.tags}
+				{#each post.tags as tag}
+					<span>&num;{tag}</span>
+				{/each}
+			{/if}
+			{#if post.description}
+				<p>{post.description}</p>
+			{/if}
+		</li>
+	{/each}
+</ul>
