1641e5929346d78fcecc95750cb0999cc610f671
y0nei <y0nei@proton.me>
2024-12-03 17:38:57 +0100

    feat: Allow route wide access to Git utils via `layout.server.ts`

--- src/routes/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import type { PageServerLoad } from "./$types";
-import { getGitCommitHash } from "$lib/utils/getCommitHash";
-
-export const load: PageServerLoad = async () => {
-	return {
-        gitCommitHash: getGitCommitHash()
-	};
-};
