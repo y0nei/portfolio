3a57acd75e3535d117c2c963d745d01d1e57bd7a
y0nei <y0nei@proton.me>
2024-12-03 17:39:41 +0100

    feat: Return GitHub file source on blog post viewing route

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,21 +1,27 @@
 import { error } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
 
-export const load: PageLoad = async ({ params, url }) => {
+export const load: PageLoad = async ({ params, url, parent }) => {
+	// BUG: Importing the function directly casues a race condition(?)
+	const { gitHTTPUrl } = await parent();
 	const collection = url.searchParams.get("collection");
 
 	try {
-		let post;
+		let post, path = "";
 		// NOTE: Ternary operator didn't work inside import string
 		if (collection) {
-			post = await import(`$posts/${collection}/${params.slug}.md`)
+			post = await import(`$posts/${collection}/${params.slug}.md`);
+			path = "/" + collection;
 		} else {
-			post = await import(`$posts/${params.slug}.md`)
+			post = await import(`$posts/${params.slug}.md`);
 		}
+
+		const gitFileSource = gitHTTPUrl + "/posts" + path + url.pathname.replace("/blog", "") + ".md";
 
 		return {
 			content: post.default,
-			meta: post.metadata
+			meta: post.metadata,
+			source: gitFileSource,
 		}
 	} catch (e) {
 		error(404, `Post ${params.slug} not found`)
