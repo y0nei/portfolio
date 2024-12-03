4cf66f24ea41cb5835fc95370bff62aa5d3cf140
y0nei <y0nei@proton.me>
2024-12-03 13:39:46 +0100

    feat: Use global layout file and .scss file for loading variables
    This allows CSS variable autocompletion to work along with applying global styles for all routes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import "minireset.css";
+    import "$lib/variables.scss";
+
+	let { children } = $props();
+</script>
+
+{@render children()}
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import "minireset.css";
     import Sections from "$lib/layouts/Sections.svelte";
     import Hero from "$lib/layouts/Hero.svelte";
 	import BentoGrid from "$lib/layouts/BentoGrid.svelte";
@@ -21,26 +20,3 @@
 		</div>
 	</section>
 </Sections>
-
-<style lang="scss">
-    @font-face {
-		font-family: "Rubik";
-		src: url("/fonts/Rubik-VariableFont_wght.ttf") format("truetype");
-		font-display: swap;
-	}
-
-	:root {
-        /* Other cool colors: #233531 #F3EEE8 */
-
-		--clr-accent: #BBD38B;
-        --clr-accent-dark: #30422E;
-        --clr-secondary: #8BBBD3;  /* Triadic to accent */
-
-		--clr-offwhite: #F9F4EA;
-		--clr-background: #1F1F21;
-		--clr-background-alt: #3A393B;
-
-		--clr-text: var(--clr-offwhite);
-        --border-radius: 0.75rem;
-	}
-</style>
