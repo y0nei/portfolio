46f43ae8c035700b27a907532e58c461ed23a917
y0nei <y0nei@proton.me>
2024-12-05 21:17:30 +0100

    feat: Markdown code block syntax highlighting with shiki

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,28 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from "svelte-adapter-bun";
 import { sveltePreprocess } from "svelte-preprocess";
-import { mdsvex } from "mdsvex";
+import { mdsvex, escapeSvelte } from "mdsvex";
+import { createHighlighter } from "shiki";
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+	extension: ".md",
+	highlight: {
+		highlighter: async (code, lang) => {
+			const highlighter = await createHighlighter({
+				themes: ["github-dark"],
+				langs: [lang]
+			})
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang: lang, theme: "github-dark" }))
+			return `{@html \`${html}\` }`
+		}
+	}
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: [".svelte", ".md"],
-	preprocess: [sveltePreprocess(), mdsvex({extension: ".md"})],
+	preprocess: [sveltePreprocess(), mdsvex(mdsvexOptions)],
 	kit: {
 		adapter: adapter(),
 		alias : {
