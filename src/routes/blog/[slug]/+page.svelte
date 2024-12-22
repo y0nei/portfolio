79ef69d39f57e52c16d38264111db9360ebfaab0
y0nei <y0nei@proton.me>
2024-12-22 16:49:35 +0100

    feat: Add spacer with reading time between article content & header

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -40,6 +40,10 @@
 		</ul>
 	{/if}
 
+	<div class="spacer">
+		<span>{data.meta.readingTime.text}</span>
+	</div>
+
 	<div class="content">
 		<data.content />
 	</div>
@@ -115,8 +119,39 @@
 			}
 		}
 
-		.content {
-			margin-top: 3rem;
+		.spacer {
+			display: flex;
+			position: relative;
+			justify-content: center;
+			user-select: none;
+			margin-top: 2rem;
+			margin-bottom: 3rem;
+
+			&::before {
+				content: "";
+				position: absolute;
+				bottom: -2px;
+				width: 100%;
+				height: 1px;
+				background: linear-gradient(
+					to right,
+					transparent 0%,
+					var(--clr-bento-border) 15%,
+					var(--clr-bento-border) 95%,
+					transparent 100%
+				);
+			}
+
+			span {
+				position: absolute;
+				top: -0.5rem;
+				font-family: "Mononoki", monospace;
+				font-size: 0.75rem;
+				padding: 0.1rem 0.3rem;
+				border: 1px solid var(--clr-bento-border);
+				background-color: var(--clr-background-alt);
+				border-radius: calc(var(--border-radius) / 2);
+			}
 		}
 
 		@media only screen and (max-width: 500px) {
