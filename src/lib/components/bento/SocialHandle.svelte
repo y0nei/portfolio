33629855acbce9f02327533fb7d34cd20f084947
y0nei <y0nei@proton.me>
2025-04-20 12:02:11 +0200

    feat: "Copied" text button feedback

--- src/lib/components/bento/SocialHandle.svelte
+++ src/lib/components/bento/SocialHandle.svelte
@@ -5,13 +5,24 @@
 
     let copynotif: HTMLElement;
     function copiedAnimation() {
-        copynotif.querySelector(".svg-container:first-child")?.animate(
+        let randomRotation = Math.floor(Math.random() * (3 - -3 + 1)) - 3;
+        copynotif.querySelector(".svg-container:first-of-type")?.animate(
             [ { opacity: 1 }, { opacity: 0 }, { opacity: 0 }, { opacity: 0 } ],
 			{ duration: 600, direction: 'alternate', iterations: 2 },
         )
-        copynotif.querySelector(".svg-container:last-child")?.animate(
+        copynotif.querySelector(".svg-container:last-of-type")?.animate(
             [ { opacity: 0 }, { opacity: 1 }, { opacity: 1 }, { opacity: 1 } ],
 			{ duration: 450, direction: 'alternate', iterations: 2, delay: 150 },
+        )
+        copynotif.getElementsByClassName("copiedtext")[0].animate(
+            [
+                { transform: `translateY(-1em) rotate(${randomRotation/3}deg)`, opacity: 0 },
+                { transform: `translateY(-1.9em) rotate(${randomRotation/2.5}deg)`, opacity: 1 },
+                { transform: `translateY(-2.05em) rotate(${randomRotation/2}deg)`, opacity: 1 },
+                { transform: `translateY(-2.15em) rotate(${randomRotation/1.25}deg)`, opacity: 1 },
+                { transform: `translateY(-2.33em) rotate(${randomRotation}deg`, opacity: 0 }
+            ],
+			{ duration: 1200 },
         )
     }
 
@@ -31,6 +42,7 @@
         <button bind:this={copynotif} use:copy={value} onclick={copiedAnimation}>
             <Icon name="copy" size={16} />
             <Icon name="check" size={20} />
+            <span class="copiedtext" aria-hidden="true">Copied</span>
         </button>
     </span>
 </div>
@@ -48,7 +60,7 @@
         font-family: "Mononoki", monospace;
         font-size: 1rem;
 
-        span {
+        & > span {
             display: inherit;
             align-items: inherit;
             justify-content: center;
@@ -76,10 +88,23 @@
             background-color: var(--clr-pill-button);
             margin-left: 3px;
 
+            .copiedtext {
+                position: absolute;
+                left: -50%;
+                top: 0;
+                right: 0;
+                text-transform: uppercase;
+                font-family: "Rubik", sans-serif;
+                font-weight: 500;
+                margin-inline: auto;
+                opacity: 0;
+                color: var(--clr-offwhite);
+            }
+
             .svg-container {
                 position: absolute;
 
-                &:last-child {
+                &:last-of-type {
                     opacity: 0;
 
                     svg { color: var(--clr-accent) };
